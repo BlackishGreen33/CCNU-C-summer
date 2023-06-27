935b5ee43c3bcb493b705b7c9e88fc8f2250e52c
墨綠BG <s5460703@gmail.com>
2023-06-27 11:22:47 +0800

    Update 5B.c

--- 函数/5B.c
+++ 函数/5B.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 #include<string.h>
-void fun(char *p){
+void fun(char* p){
     int max_idx = 0;
     for(int i = 1; i < strlen(p); i++)
-        if (p[i] > p[max_idx]) max_idx = i;
+        if(p[i] > p[max_idx]) max_idx = i;
     char temp = p[max_idx];
     for(int i = max_idx; i > 0; i--) p[i] = p[i - 1];
     p[0] = temp;
 }
 int main(){
-    char str[8];
-    for(int i = 0; i < 8; i++) scanf("%c", &str[i]);
+    char str[9];
+    for(int i = 0; i < 9; i++) scanf("%c", &str[i]);
+    str[8] = '\0';
     printf("Before calling fun: %s\n", str);
     fun(str);
     printf("After calling fun: %s", str);
